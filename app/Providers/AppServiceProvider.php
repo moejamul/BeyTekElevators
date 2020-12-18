0bf2d95d6a966d21c873eb8ead6fa261d288a232
moejamul <mohjamul@gmail.com>
2020-12-18 22:22:26 +0200

    added default string length to avoid errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191); //Setting String Length by default
+        //if we don't include this we will get an error
     }
 }
